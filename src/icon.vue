93bdea386db6c2ffdc553c6c2f1b7289b004d66b
CCCLARITY <1227039581@qq.com>
2019-12-22 22:30:09 +0800

    button

---
+++ src/icon.vue
@@ -0,0 +1,24 @@
+<template>
+    <svg v-if="name" class="icon">
+        <use :xlink:href="`#i-${name}`"></use>
+    </svg>
+</template>
+<script>
+    export default {
+        props: ['name'],
+        data: function () {
+            return{
+
+            }
+        },
+        mounted() {
+
+        }
+    }
+</script>
+<style>
+    .icon{
+        width: 1em;
+        height: 1em;
+    }
+</style>
