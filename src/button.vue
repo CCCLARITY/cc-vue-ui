a6c3437cef0dd5dc441cdc9c1950d437cc521895
CCCLARITY <1227039581@qq.com>
2019-12-22 22:29:04 +0800

    完善button组件

--- src/button.vue
+++ src/button.vue
@@ -1,27 +1,61 @@
 <template>
-    <button class="g-button">CCCLARITY</button>
+    <button class="g-button" @click="this.xxx" :class="{[`icon-${iconPosition}`]: true}">
+        <g-icon :name="icon" v-if="icon && !loading" class="icon"></g-icon>
+        <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
-    export default {}
+export default {
+    // props: ['icon', 'iconPosition'],
+    props: {
+        icon: {},
+        loading: {
+            type: Boolean,
+            default: false,
+        },
+        iconPosition: {
+            type: String,
+            default: 'left',
+            validator(value){
+                return value === 'left' || value === 'right';
+            },
+        }
+    },
+    methods: {
+        xxx(){
+            this.$emit('yyy');
+        }
+    },
+    mounted() {
+        console.log('this.$options.propsData.icon', this.$options.propsData.icon)
+    }
+}
 </script>
 
 <style lang="scss">
-    .g-button{
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
-        &:hover{
-            border-color: var(--border-color-hover);
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+    .g-button {
+        font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
+        border-radius: var(--border-radius); border: 1px solid var(--border-color); background: var(--button-bg);
+        display: inline-flex; justify-content: center; align-items: center; vertical-align: middle;
+        > .icon{ order: 1; margin-right: .1em;}
+        > .content{ order: 2; }
+        &.icon-right{
+            > .icon{ order: 2; margin-right: 0; margin-left: .1em;}
+            > .content{ order: 1; }
         }
-        &:active{
-            background: var(--button-ative-bg);
-        }
-        &:focus{
-            outline: none;
+        &:hover { border-color: var(--border-color-hover); }
+        &:active { background: var(--button-ative-bg); }
+        &:focus { outline: none; }
+        .loading{
+            animation: spin 2s infinite linear;
         }
     }
 </style>
