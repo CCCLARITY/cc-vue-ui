c8985387b29ce883e7a4dccd7945ca68dd5ac45f
CCCLARITY <1227039581@qq.com>
2019-12-23 23:07:46 +0800

    给button的icon属性加单元测试

--- src/app.js
+++ src/app.js
@@ -13,3 +13,24 @@ new Vue({
         loading1: false,
     }
 });
+
+import chai from 'chai';
+const expect = chai.expect;
+
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+
+    const Constructor = Vue.extend(Button);         //TODO
+    const vm = new Constructor({
+        propsData: {                                //TODO
+            icon: 'settings',
+        }
+    });
+    vm.$mount(div);
+    let el = vm.$el.querySelector('use');       //TODO
+    expect(el.getAttribute('xlink:href')).to.eq('#i-settings');
+
+    vm.$el.remove();            //TODO
+    vm.$destroy();
+}
