f440094610e70fde75e0300c15aac23f11c07318
CCCLARITY <1227039581@qq.com>
2019-12-23 23:08:18 +0800

    icon属性加单元测试

---
+++ src/button-group.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        mounted() {
+            for(let node of this.$el.children){
+                if(node.nodeName.toLowerCase() !== 'button'){
+                    console.error('应该输入g-button，您输入的是' + node.nodeName.toLowerCase())
+                }
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    .g-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        > .g-button{
+            border-radius: 0;
+            margin-left: -1px;
+            &:first-child{
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover{
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
